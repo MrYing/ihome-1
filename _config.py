50269deadbc86e120b083bfea3b1e383a3bf46ba
IIce <1782509343@qq.com>
2019-05-29 16:38:50 +0800

    项目初始化及数据库设计

---
+++ _config.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/5/29 16:34
+# @Author  : gao
+# @File    : _config.py
---
+++ ihome/api_1_0/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/5/29 9:25
+# @Author  : gao
+# @File    : __init__.py.py
+
+from flask.blueprints import Blueprint
+
+# 需求url:127.0.0.1:5000/api/1.0/index
+api = Blueprint('api_1_0', __name__, url_prefix='/api/1.0')
